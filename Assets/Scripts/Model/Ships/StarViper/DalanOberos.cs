5cdca990a22b39e9ee7a811abb8f5040f138b218
Hunter <hkeaton0727@gmail.com>
2018-01-06 13:12:52 -0500

    -Added DalanOberos (nonfunctioning) -Added PrinceXizor (nonfunctioning)

---
+++ Assets/Scripts/Model/Ships/StarViper/DalanOberos.cs
@@ -0,0 +1,96 @@
+﻿using Abilities;
+using Movement;
+using SubPhases;
+using System;
+
+namespace Ship
+{
+    namespace StarViper
+    {
+        public class DalanOberos : StarViper
+        {
+            public DalanOberos() : base()
+            {
+                PilotName = "Dalan Oberos";
+                PilotSkill = 6;
+                Cost = 30;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+                PilotAbilities.Add(new DalanOberosAbility());
+            }
+        }
+    }
+}
+
+namespace Abilities
+{
+    public class DalanOberosAbility : GenericAbility
+    {
+
+        public override void ActivateAbility()
+        {
+            HostShip.OnActivationPhaseStart += RegisterDalanAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActivationPhaseStart -= RegisterDalanAbility;
+        }
+
+        private void RegisterDalanAbility(Ship.GenericShip ship)
+        {
+            Triggers.RegisterTrigger(new Trigger
+            {
+                Name = Name,
+                TriggerType = TriggerTypes.OnActivationPhaseStart,
+                TriggerOwner = HostShip.Owner.PlayerNo,
+                EventHandler = AskGuriAbility
+            });
+        }
+
+        private void AskGuriAbility(object sender, EventArgs e)
+        {
+            if (HostShip.HasToken(typeof(Tokens.StressToken)))
+            {
+                Triggers.FinishTrigger();
+                return;                
+            }
+
+            GenericMovement shipMovement = HostShip.AssignedManeuver;
+            if (shipMovement is TurnMovement ||
+                shipMovement is BankMovement ||
+                shipMovement is SegnorsLoopMovement)
+            {              
+                DecisionSubPhase decisionSubPhase = (DecisionSubPhase)Phases.StartTemporarySubPhaseNew(
+                    Name,
+                    typeof(DecisionSubPhase),
+                    null);
+
+                decisionSubPhase.InfoText = "Would you like to perform a Red Tallon Roll of the same maneuver?";
+
+                decisionSubPhase.AddDecision("Yes", delegate 
+                {
+                    PerformTallonRoll(shipMovement);
+                    DecisionSubPhase.ConfirmDecisionNoCallback();
+                    Phases.CurrentPhase.StartPhase();
+                });
+
+                decisionSubPhase.AddDecision("No", delegate { Triggers.FinishTrigger(); });
+
+                decisionSubPhase.ShowSkipButton = true;
+
+                decisionSubPhase.Start();
+                Triggers.FinishTrigger();
+
+            }
+        }
+
+        private void PerformTallonRoll(GenericMovement shipMovement)
+        {
+            HostShip.SetAssignedManeuver(new TallonRollMovement(shipMovement.Speed, shipMovement.Direction, shipMovement.Bearing, Movement.ManeuverColor.Red));
+        }
+    }
+}
+
---
+++ Assets/Scripts/Model/Ships/StarViper/PrinceXizor.cs
@@ -0,0 +1,95 @@
+﻿
+using Abilities;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace StarViper
+    {
+        public class PrinceXizor : StarViper
+        {
+            public PrinceXizor() : base()
+            {
+                PilotName = "Prince Xizor";
+                PilotSkill = 7;
+                Cost = 31;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+                PilotAbilities.Add(new PrinceXizorAbility());
+            }
+        }
+    }
+}
+
+namespace Abilities
+{
+    public class PrinceXizorAbility : GenericAbility
+    {
+
+        public override void ActivateAbility()
+        {
+            HostShip.OnAttackHitAsDefender += RegisterPrinceXizorAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnAttackHitAsDefender -= RegisterPrinceXizorAbility;
+        }
+
+        private void RegisterPrinceXizorAbility()
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnAttackHit, AskShedDamage);
+        }
+
+        private void AskShedDamage(object sender, EventArgs e)
+        {
+            List<GenericShip> nearbyFriendlyShips = Board.BoardManager.GetShipsAtRange(HostShip, new Vector2(1, 1), Team.Type.Friendly);
+            if (nearbyFriendlyShips.Count > 1)
+            {
+                SelectTargetForAbility(
+                    ShedDamage,
+                    new List<TargetTypes> { TargetTypes.OtherFriendly },
+                    new Vector2(1, 1));
+            }
+        }
+
+        private void ShedDamage()
+        {
+            int successes = Combat.DiceRollAttack.Successes;
+            int criticalSuccesses = Combat.DiceRollAttack.CriticalSuccesses;
+
+            DecisionSubPhase decisionSubPhase = (DecisionSubPhase)Phases.StartTemporarySubPhaseNew(
+                Name,
+                typeof(DecisionSubPhase),
+                DecisionSubPhase.ConfirmDecision);
+
+            decisionSubPhase.InfoText = string.Format("Which type of damage would you like to apply to {0}?", TargetShip);
+
+            if (successes > 0)
+            {
+                decisionSubPhase.AddDecision("Apply 1 Success?", delegate { MitigateDamage(DiceKind.Attack); });
+
+            }
+            if (criticalSuccesses > 0)
+            {
+                decisionSubPhase.AddDecision("Apply 1 Critical Success?", delegate { MitigateDamage(DiceKind.Attack); });
+            }
+
+            decisionSubPhase.ShowSkipButton = true;
+
+            decisionSubPhase.Start();
+        }
+
+        private void MitigateDamage(DiceKind die)
+        {
+
+        }
+    }
+}
+
